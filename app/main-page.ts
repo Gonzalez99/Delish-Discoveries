f9b54127db7823e6a7a00c9790dcb0c705b817a6
Gonzalez99 <77453735+Gonzalez99@users.noreply.github.com>
2023-06-28 23:15:55 +0200

    2 files modified

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,5 +1,28 @@
 import { EventData, Page } from '@nativescript/core';
 import { HelloWorldModel } from './main-view-model';
+import { ActionItem } from '@nativescript/core';
+
+// ...
+
+showPopupMenu(args) {
+  const actionItem = args.object as ActionItem;
+  const actionBar = actionItem.actionBar;
+
+  const popupMenu = new PopupMenu();
+
+  const menuItem1 = new MenuItem('Menu Item 1');
+  const menuItem2 = new MenuItem('Menu Item 2');
+  const menuItem3 = new MenuItem('Menu Item 3');
+
+  popupMenu.items.push(menuItem1, menuItem2, menuItem3);
+
+  popupMenu.on('menuItemTap', (menuItem) => {
+    // Handle menu item tap event here
+    console.log('Selected menu item:', menuItem.title);
+  });
+
+  popupMenu.show(actionBar);
+}
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object;
